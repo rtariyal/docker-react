f4343f484f8d79e36323b51b6044f22d13f30fb5
Rishabh Tariyal <rishabhtariyal@gmail.com>
2021-01-24 13:37:52 +0000

    added travis file

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <div>
           Edit <code>src/App.js</code> and save to reload.
           <p>Bye there!!</p>
-        </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
